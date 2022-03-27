36dd999ee8f3da20aeec79a7c31178f4ae9dd89b
Carlos Vargas <varlopecar@gmail.com>
2022-03-27 15:45:58 -0600

    Creating Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import './App.css'
+import Card from './components/Card'
 
 function App() {
   return (
-    
+    <Card />
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import './Card.css'
+
+const Card = () => {
+    return <div>
+        <h1 className='title'>Hola</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ipsa? Rerum mollitia minima ipsam unde atque cupiditate, explicabo corrupti harum!
+        </p>
+    </div>
+}
+
+export default Card
