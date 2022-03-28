714af07e1268c64f3b6dcbac0dd9cec201bbcb63
Carlos Vargas <varlopecar@gmail.com>
2022-03-28 06:25:49 -0600

    Button made. Working on design

--- src/components/Advices/Advice.jsx
+++ src/components/Advices/Advice.jsx
@@ -1,19 +1,24 @@
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import "./Advice.css";
 
 const Advice = (props) => {
   return (
     <Card className="card">
-      <div className="advice-number">ADVICE NUMBER #123</div>
+      <div className="advice-number">ADVICE #123</div>
       <div className="advice-description">
         <p className="text-description">
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          consectetur quos pariatur, dolor nisi ab consequatur expedita.
-          Facilis, odit recusandae?"
+          consectetur quos pariatur"
         </p>
       </div>
-      <img
-      className="pattern" src="src\assets\images\pattern-divider-desktop.svg" />
+      <div>
+        <img
+          className="pattern"
+          src="src\assets\images\pattern-divider-desktop.svg"
+        />
+      </div>
+      <Button />
     </Card>
   );
 };
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.css";
+
+const Button = () => {
+    return (
+        <div>
+            <button className="button"><img src="src\assets\images\icon-dice.svg" alt="" /></button>
+        </div>
+    )
+}
+
+export default Button;
