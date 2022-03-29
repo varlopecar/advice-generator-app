21bb8ff040f63235c4afe73199eb297841306bc3
Carlos Vargas <varlopecar@gmail.com>
2022-03-29 09:37:12 -0600

    Working with the fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import Advice from './components/Advices/Advice'
+import React, { useState, useEffect } from 'react';
+import Advice from "./components/Advices/Advice";
 
 function App() {
+  const [advice, setAdvice] = useState(null);
+
+  const url = "https://api.adviceslip.com/advice";
+
+  const fetchAdvice = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setAdvice(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchAdvice(url);
+  }, []);
+
   return (
-    <Advice />
-  )
+    <Advice advice={advice} />
+  ) 
 }
 
-export default App
+export default App;
--- src/components/Advices/Advice.jsx
+++ src/components/Advices/Advice.jsx
@@ -2,20 +2,19 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./Advice.css";
 
-const Advice = (props) => {
+const Advice = ({advice}) => {
+  console.log(advice.slip.id);
   return (
-    <Card className="card">
-      <div className="advice-number">ADVICE #123</div>
+    <Card>
+      <div className="advice-number">ADVICE #{advice.slip.id}</div>
       <div className="advice-description">
         <p className="text-description">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          consectetur quos pariatur"
+        "{advice.slip.advice}"
         </p>
       </div>
       <div>
         <img
           className="pattern"
-          src="src\assets\images\pattern-divider-desktop.svg"
         />
       </div>
       <Button />
